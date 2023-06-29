6e058984acd20f06166455df93f00773e11cc3c5
chrispy <chrissper@web.de>
2023-06-29 23:19:13 +0000

    git_push_clear.lua: Git Push Inception

--- git_push_clear.lua
+++ git_push_clear.lua
@@ -1,1 +1,20 @@
-filename = arg[1]password = ""print("Commit Nachricht für "..filename..": ")commit_msg = read()file = fs.open(filename, "r")content = file.readAll()file.close()msg = password.."\n"..filename.."\n"..filename..": "..      commit_msg.."\n"..contentaddress = "http://82.165.177.70:8000"http.post(address, msg)print("Pray to God it worked /\\")
+
+filename = arg[1]
+password = ""
+
+print("Commit Nachricht für "..filename..": ")
+commit_msg = read()
+
+file = fs.open(filename, "r")
+content = file.readAll()
+file.close()
+
+msg = password.."\n"..filename.."\n"..filename..": "..
+      commit_msg.."\n"..content
+address = "http://82.165.177.70:8000"
+
+http.post(address, msg)
+print("Pray to God it worked /\\")
+
+
+
