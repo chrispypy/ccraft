25269b3dc3b30c2bbcb8ecae8048f88f0ffe2a35
chrispy <chrissper@web.de>
2023-06-30 17:56:37 +0000

    IC zuckerrohr

---
+++ zuckerrohr.lua
@@ -0,0 +1,19 @@
+os.loadAPI("/mods/H")
+
+L = 15
+cane = "minecraft:sugar_cane"
+
+M.fGo("uulffr")
+for j=1,2 do
+  for i=1,L do
+    M.fGo("f")
+    if j==2 an
+       not turtle.detectDown() and
+       H.selectItem( cane ) then
+      turtle.placeDown()
+    end
+  end
+  M.fGo("rrd")
+end
+M.fGo("rffl")
+
